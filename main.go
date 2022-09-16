bd0570d631ab6d3f4201102a30195b8ff7ffbfc7
vika <757058941@qq.com>
2022-09-16 16:12:31 +0800

    update gin setting

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"gin-framework/basic/src/config"
-	db "gin-framework/basic/src/database"
-	"gin-framework/basic/src/router"
 	"gin-framework/basic/src/setting"
 
 	"github.com/gin-gonic/gin"
@@ -13,10 +11,6 @@ func main() {
 	r := gin.Default()
 	//初始化设置
 	setting.Init(r)
-	//链接数据库
-	db.ConnectDb()
-	//设置路由
-	router.SetRouter(r)
 	//启动服务
 	r.Run(config.ServerPort)
 }
--- src/setting/setting.go
+++ src/setting/setting.go
@@ -1,13 +1,17 @@
 package setting
 
 import (
+	db "gin-framework/basic/src/database"
 	"gin-framework/basic/src/middleware"
+	"gin-framework/basic/src/router"
 	"gin-framework/basic/src/schedule"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Init(r *gin.Engine) {
+	db.ConnectDb()                   //链接数据库
+	router.SetRouter(r)              //设置路由
 	r.Use(middleware.LoggerToFile()) //开启日志中间件
 	schedule.Start()                 //开始定时任务
 }
