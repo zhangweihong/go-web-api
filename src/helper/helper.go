5a70aaab343cb7bdc50a07312d6ff273c5ec1426
vika <757058941@qq.com>
2022-09-16 16:10:16 +0800

    增加 高效拼接字符串 Splicing

--- src/helper/helper.go
+++ src/helper/helper.go
@@ -1,11 +1,22 @@
 package helper
 
 import (
-	"fmt"
 	"os"
 	"path"
 	"strings"
 )
+
+var sBuild strings.Builder
+
+//高效拼接字符串
+func Splicing(strs ...string) string {
+	sBuild.Reset()
+	len := len(strs)
+	for i := 0; i < len; i++ {
+		sBuild.WriteString(strs[i])
+	}
+	return sBuild.String()
+}
 
 //捕获异常
 func Try(fun func(), catch func(interface{})) {
@@ -22,7 +33,8 @@ var upLoadDir = "upload/"
 //获取上传的文件路径
 func GetUploadsFilePath(relPath string) string {
 	curDir, _ := os.Getwd()
-	var p = fmt.Sprintf("%v/%v%v", curDir, upLoadDir, relPath)
+	p := Splicing(curDir, "/", upLoadDir, relPath)
+	// var p = Splicing("%v/%v%v", curDir, upLoadDir, relPath)
 	p = strings.ReplaceAll(p, "\\", "/")
 	dir := path.Dir(p)
 	_, err := os.Stat(dir)
